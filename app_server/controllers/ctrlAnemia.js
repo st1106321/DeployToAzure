e054b465713848b6891a8dc8d311b39a6147f3a4
st1106321 <1106321@laurea.fi>
2018-04-19 06:03:52 +0300

    8. post API +1

--- app_server/controllers/ctrlAnemia.js
+++ app_server/controllers/ctrlAnemia.js
@@ -3,6 +3,33 @@
 const request = require('request');
 const apiURL = require('./apiURLs');
 
+const showForm = function (req, res) {
+    res.render('anemia_add');
+};
+
+const addData = function (req, res) {
+    const path = '/api/anemia';
+    const postData = {
+        type: req.body.type,
+        name: req.body.name
+    }
+    const requestOptions = {
+        url: apiURL.server + path,
+        method: 'POST',
+        json: postData
+    };
+    request(
+        requestOptions,
+        function (err, response) {
+            if (response.statusCode === 201) {
+                res.redirect('/anemia');
+            } else {
+                res.render('error', {message: `Error adding data: ${response.statusMessage} (${response.statusCode}) `});
+            }
+        }
+    )
+};
+
 const anemiaSolutions = function (req, res) {
     const path = '/api/anemia';
     const requestOptions = {
@@ -30,5 +57,7 @@ const anemiaSolutions = function (req, res) {
 };
 
 module.exports = {
-    anemiaSolutions
+    anemiaSolutions,
+    showForm,
+    addData
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,7 +4,6 @@ const router = express.Router();
 /* Declaring the controllers*/
 const ctrlMain = require('../controllers/ctrlMain');
 const ctrlAnemia = require('../controllers/ctrlAnemia');
-// const ctrlImmune = require('../controllers/ctrlImmune');
 const ctrlInsomnia = require('../controllers/ctrlInsomnia');
 const ctrlLibido = require('../controllers/ctrlLibido');
 
@@ -12,8 +11,13 @@ const ctrlLibido = require('../controllers/ctrlLibido');
 
 router.get('/', ctrlMain.index);
 router.get('/anemia', ctrlAnemia.anemiaSolutions);
-// router.get('/immune', ctrlImmune.immuneSolutions);
 router.get('/insomnia', ctrlInsomnia.insomniaSolutions);
 router.get('/libido', ctrlLibido.libidoSolutions);
+
+router
+    .route('/anemia/add')
+    .get(ctrlAnemia.showForm)
+    .post(ctrlAnemia.addData);
+
 
 module.exports = router;
